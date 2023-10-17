3cf0144d2888d6b6556a2ba977b6316ac9f6cc83
in-deepaknegi <negideepak.in@gmail.com>
2023-10-17 11:58:15 +0530

    Intial commit, configured folder management

---
+++ components/Home/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Hero from './Hero'
+import About from './About'
+import Footer from '../Footer'
+
+const Main = () => {
+  return (
+    <>
+        <Navbar />
+        <Hero />
+        <About />
+        <Footer />
+    </>
+  )
+}
+
+export default Main
