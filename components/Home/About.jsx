78d780dd14752eaa42c697aed15bc8af716e8852
in-deepaknegi <negideepak.in@gmail.com>
2023-10-18 23:54:08 +0530

    updated Hero and About sections

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const About = () => {
     return (
-        <section>About</section>
+        <section className='relative overflow-hidden pt-24 pb-20'>
+            <div className='max-w-7xl mx-auto px-8'>
+                <div className='mx-12'>
+                    <div className='grid grid-cols-2 gap-16 items-center lg:mx-0'>
+                        <h2 className='text-5xl max-w-lg font-medium'>
+                            I&apos;m your <span className='gradient-cool'>all-in-one </span>
+                            project solution
+                        </h2>
+                        <p className='text-gray-700'>
+                            Experience the advantage of an all-inclusive project solution, where excellence, speed, and responsiveness converge to ensure the highest quality outcome.
+                        </p>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
