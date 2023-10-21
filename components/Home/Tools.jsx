bd1479c0e39218301c804519873627670d93735e
in-deepaknegi <negideepak.in@gmail.com>
2023-10-21 22:53:19 +0530

    added responsiveness to Work section

--- components/Home/Tools.jsx
+++ components/Home/Tools.jsx
@@ -12,8 +12,8 @@ import Figma from '@/public/tools/figma.svg'
 
 const Tools = () => {
     return (
-        <section className='max-w-7xl mx-auto px-8 pt-16'>
-            <div className='mx-12'>
+        <section className='max-w-7xl mx-auto px-3 md:px-8 pt-16'>
+            <div className='mx-3 md:mx-12'>
                 <h2 className='text-5xl font-medium tracking-wider font-sf max-w-md mb-5'>
                     The tools I use on my
                     <span className='gradient-fire'> day-to-day</span>
@@ -40,42 +40,42 @@ const Tools = () => {
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-8 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden sm:flex w-[4.5rem] h-[4.5rem] mt-8 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Reactt}
                             alt='html-5'
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-0 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden sm:flex w-[4.5rem] h-[4.5rem] mt-0 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Next}
                             alt='html-5'
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-8 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden md:flex w-[4.5rem] h-[4.5rem] mt-8 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Postgre}
                             alt='html-5'
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-16 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden md:flex w-[4.5rem] h-[4.5rem] mt-16 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Mongo}
                             alt='html-5'
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-24 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden lg:flex w-[4.5rem] h-[4.5rem] mt-24 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Firebase}
                             alt='html-5'
                             className='w-[2.8rem]'
                         />
                     </div>
-                    <div className='flex w-[4.5rem] h-[4.5rem] mt-16 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
+                    <div className='hidden lg:flex w-[4.5rem] h-[4.5rem] mt-16 bg-gray-50 border items-center justify-evenly rounded-full flex-shrink-0'>
                         <Image
                             src={Figma}
                             alt='html-5'
--- components/Home/Work.jsx
+++ components/Home/Work.jsx
@@ -4,30 +4,30 @@ import Mridakriti from '@/public/mridakriti.png'
 
 const Work = () => {
     return (
-        <section className='mt-20 font-sf'>
-            <div className='text-center'>
+        <section className='mt-20 font-sf px-3 md:px-0'>
+            <div className='mx-3 md:mx-0 text-center'>
                 <h2 className='my-4 text-5xl font-medium'>Check out my
                     <span className='gradient-full'> latest work</span></h2>
                 <p className='mx-auto max-w-3xl text-lg text-gray-600'>
                     My goal is to create effective digital experiences that make people’s lives easier and better. I hope my work is a reflection of this.
                 </p>
             </div>
-            <div className='mt-20 relative mx-[8rem] max-w-none'>
-                <div className='flex gap-4 bg-gray-100 rounded-xl shadow-xl pb-10 mb-4'>
-                    <div className='relative w-[25%] mt-10 ml-10'>
-                        <h3 className='text-3xl my-5 tracking-wider'>Mridakriti</h3>
-                        <p className='tracking-wide text-gray-700'>
+            <div className='mt-16 relative mx-12 md:mx-16 lg:mx-[8rem] max-w-none'>
+                <div className='flex lg:flex-row flex-col gap-4 bg-gray-100 rounded-xl lg:shadow-xl lg:pb-10 mb-4'>
+                    <div className='lg:w-[25%] mt-10 lg:ml-10 mx-3 md:mx-8'>
+                        <h3 className='text-3xl my-1 md:my-5 tracking-wider'>Mridakriti</h3>
+                        <p className='hidden md:block tracking-wide text-gray-700'>
                             I helped Horizons in Perspective develop a new branding and website experience, as well as an engaging product design for their bi-annual journal publication.
                         </p>
-                        <button className='mt-10 text-base text-blue-500'>
+                        <button className='hidden md:block mt-10 text-base text-blue-500'>
                             View case study
                         </button>
                     </div>
-                    <a href="#" className='w-[75%]'>
+                    <a href="#" className='lg:w-[75%] mx-3 mb-3 lg:mb-0'>
                         <Image
                             src={Mridakriti}
                             alt='mridakriti'
-                            className='mt-8 rounded-xl ml-10 shadow-lg'
+                            className='md:mt-8 rounded-xl  lg:ml-10 shadow-lg'
                         />
                     </a>
                 </div>
