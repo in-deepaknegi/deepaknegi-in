63a75c58ef9d9928e13379131f3ac625a3f9197a
in-deepaknegi <negideepak.in@gmail.com>
2023-10-25 22:25:26 +0530

    updated privacy policy

--- app/privacy/page.jsx
+++ app/privacy/page.jsx
@@ -1,40 +1,92 @@
+import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Privacy = () => {
     return (
-        <main className='font-sf'>
+        <>
             <Navbar />
-            <section className='relative max-w-7xl mx-auto px-10 w-full h-screen'>
-                <div className='mx-10'>
-                    <h1 className='mt-16 text-4xl text-center'>Privacy Policy</h1>
-                    <p className='text-gray-500 text-base mt-3'>Last Updated on October 24th, 2023</p>
+            <main className='font-sf'>
+                <section className='relative max-w-4xl mx-auto px-10 w-full mb-20'>
+                    <article className='mx-10 privacy'>
+                        <h1 className='mt-16 text-4xl text-center gradient-cool'>Privacy Policy</h1>
+                        <p >Last Updated on October 24th, 2023</p>
 
-                    <h2 className='text-4xl mt-4'>Introduction to Privacy Policy</h2>
+                        <h2>Introduction to Privacy Policy</h2>
 
-                    <p className='text-gray-500 text-base mt-3'>
-                        This privacy policy (the PRIVACY POLICY) applies to your use of the website of Deepak Negi hosted at deepaknegi.in, the Services (as defined under the Terms of Use), but does not apply to any third party websites that may be linked to them, or any relationships you may have with the businesses listed on our website.
-                    </p>
-                    <p className='text-gray-500 text-base mt-3'>
-                        The terms <strong>&quot;WE&quot;</strong>, &quot;OUR&quot; and &quot;US&quot; refer to Razorpay and the terms &quot;YOU&quot;, &quot;YOUR&quot; and &quot;USER&quot; refer to you, as a user of Razorpay. The term &quot;PERSONAL INFORMATION&quot; means information that you provide to us which personally identifies you to be contacted or identified, such as your name, phone number, email address, and any other data that is tied to such information. Our practices and procedures in relation to the collection and use of Personal Information have been set-out below in order to ensure safe usage of the Website for you.
-                    </p>
-                    <p className='text-gray-500 text-base mt-3'>
-                        It is clarified that the terms and conditions that are provided separately, form an integral part of your use of this Website and should be read in conjunction with this Privacy Policy.
-                    </p>
+                        <p >
+                            Thank you for using deepaknegi.in (&apos;we,&apos; &apos;us,&apos; or &apos;our&apos;). We are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains in detail how we collect, use, and safeguard your personal information. By using our website or app, you consent to the practices described in this policy.
+                        </p>
 
-                    <h2 className='text-4xl mt-4'>Information we collect and how we use it</h2>
-                    <p className='text-gray-500 text-base mt-3'>
-                        We collect, receive and store your Personal Information. If you provide your third-party account credentials (&apos;THIRD PARTY ACCOUNT INFORMATION&apos;) to us, you understand that some content and information in those accounts may be transmitted to your account with us if you authorise such transmissions and that Third Party Account Information transmitted to us shall be covered by this Privacy Policy. You may opt to not provide us with certain information, however that will restrict you from registering with us or availing some of our features and services.
-                    </p>
-                    <p className='text-gray-500 text-base mt-3'>
-                        We use commercially reasonable efforts to ensure that the collection of Personal Information is limited to that which is necessary to fulfill the purposes identified below. If we use or plan to use your information in a manner different than the purpose for which it is collected, then we will ask you for your consent prior to such use.
-                    </p>
-                    <p className='text-gray-500 text-base mt-3'>
-                        The Personal Information collected will be used only for the purpose of enabling you to use the services provided by us, to help promote a safe service, calibrate consumer interest in our products and services, inform you about online offers and updates, troubleshoot problems, customize User experience, detect and protect us against error, fraud and other criminal activity, collect money, enforce our terms and conditions, and as otherwise described to you at the time of collection of such information.
-                    </p>
-                </div>
-            </section>
-        </main>
+                        <h2>Information we collect and how we use it</h2>
+                        <p >
+                            We may collect various types of personal information from our website/app users. This information includes contact information such as your name, email address, phone number, or mailing address. Additionally, we may collect user-generated content, such as comments, reviews, or forum contributions, which may reveal insights about your preferences and interests.
+                        </p>
+                        <p >
+                            Our website also gathers technical information, such as your IP address, browser type, device type, and operating system. This information helps us understand the technical environment you&apos;re using to access our platform. Furthermore, we collect usage data to gain insights into your interactions with our website. This data includes the pages you visit, the duration of your visits, and your engagement with our content.
+                        </p>
+
+                        <h2>How we Collect Information</h2>
+                        <p>
+                            We employ several methods for collecting personal information
+                        </p>
+                        <ul className='list-disc ml-10'>
+                            <li>
+                                Providing Services: We use your information to deliver the services and features you&apos;ve requested, including user accounts, personalized experiences, and access to specific content.
+                            </li>
+                            <li>
+                                Communication: We may use your contact information to send you important updates, newsletters, promotional materials, and information related to your account or transactions.
+                            </li>
+                            <li>
+                                Customization: By analyzing your usage data and user-generated content, we personalize your experience by tailoring content to your interests, enhancing the quality of your interactions with our platform.
+                            </li>
+                            <li>
+                                Analytics: We continually strive to improve our website&apos;s performance and user experience. We analyze data on your visits, interactions, and the functionality of our platform to make necessary enhancements.
+                            </li>
+                            <li>
+                                Legal Compliance: To ensure adherence to applicable laws and regulations, we may use your personal information for legal and regulatory compliance purposes.
+                            </li>
+                        </ul>
+
+                        <h2>
+                            Keeping Information Safe
+                        </h2>
+                        <p>
+                            We take the security of your personal information seriously. Our team has implemented robust security measures to protect your data from unauthorized access, disclosure, alteration, and destruction. However, it&apos;s important to note that while we make every effort to secure your information, no method of data transmission over the internet is entirely secure. Thus, while we take precautions, we cannot guarantee the absolute security of your information.
+                        </p>
+                        <h2>
+                            Information Sharing with Third Parties
+                        </h2>
+                        <p>
+                            We respect your privacy and do not sell, trade, or otherwise transfer your personal information to external parties without your explicit consent. There are limited exceptions to this rule:
+                        </p>
+                        <ul className='list-disc ml-10'>
+                            <li>
+                                Service Providers: We may share your information with trusted service providers to help us operate our website/app and provide you with the services you expect. These third parties are bound by strict confidentiality agreements.
+                            </li>
+                            <li>
+                                Legal Obligations: In certain circumstances, we may share your information to comply with legal obligations, enforce our site policies, or protect our rights, property, or safety, or those of others. We may also share information in the event of a business transaction such as a merger, acquisition, or sale.
+                            </li>
+                        </ul>
+
+                        <h2>
+                            Changes to This Policy
+                        </h2>
+                        <p>
+                            We may update this Privacy Policy from time to time. When we make changes, we will revise the date at the top of this page. We recommend reviewing this policy periodically to stay informed about how we are protecting and using your information.
+                        </p>
+
+                        <h2>Contact Us</h2>
+                        <p>
+                            If you have any questions or concerns regarding this Privacy Policy or your personal information, please contact us at [Your Contact Information]. We value your privacy and are here to assist you with any inquiries or requests.
+                            <br />
+                            By using our website, you agree to this Privacy Policy and our Terms of Service.
+                        </p>
+                    </article>
+                </section>
+            </main>
+            <Footer />
+        </>
     )
 }
 
