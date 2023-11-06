091c6d3f62920594fbd0498446d6d127f55c27c9
in-deepaknegi <negideepak.in@gmail.com>
2023-11-06 15:01:30 +0530

    google auth -v2

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectUser } from '../../../lib/mongodbUser'
+import { connectUser } from '../../../lib/dbUser'
 import User from '../../../models/User'
 
 export async function POST(request) {
