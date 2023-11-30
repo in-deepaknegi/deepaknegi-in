127a650cb5f704a5a0bd45c3398b3c45f58e7dd2
in-deepaknegi <negideepak.in@gmail.com>
2023-11-30 10:01:59 +0530

    added f-s for works and blogs

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import Hero from '@/pages/about/Hero'
+
+export const metadata: Metadata = {
+    title: 'About',
+    description: 'About page',
+}
+
+const page = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </>
+    )
+}
+
+export default page
