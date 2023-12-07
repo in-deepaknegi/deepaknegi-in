7ec322db279fc379d6e0c57e9297a556f8a506d5
in-deepaknegi <negideepak.in@gmail.com>
2023-12-07 14:57:00 +0530

    updated login page

---
+++ pages/login/Hero.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Image from 'next/image'
+import GoogleLogo from '@/public/google-logo.svg';
+
+const Hero = () => {
+    return (
+        <main className="relative flex flex-1 flex-col overflow-hidden px-4 py-1 sm:px-6 lg:px-8">
+            <div className='relative mx-10 mt-10 w-10'>
+                <a href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" className='text-gray-700 border rounded-full' stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m12 19-7-7 7-7" /><path d="M19 12H5" /></svg>
+                </a>
+            </div>
+            <div className="relative flex flex-1 flex-col items-center justify-center pt-12">
+                <a href='/' className='flex mx-auto mb-10 w-auto'>
+                    <div className='my-auto ml-3 text-2xl'>Deepak Negi</div>
+                </a>
+
+                <div className='max-w-sm w-full'>
+                    <button className='inline-flex justify-center rounded-lg text-base py-2.5 px-4 w-full border'>
+                        <Image
+                            src={GoogleLogo}
+                            alt='google-logo'
+                            width={20}
+                            height={20}
+                            className='mx-3'
+                        />
+                        Log in with Google
+                    </button>
+                </div>
+                <div className='flex items-center  max-w-sm my-4'>
+                    <div className='w-[24rem] h-[0.025rem] bg-gray-200'></div>
+                    <div className='mx-2'>or</div>
+                    <div className='w-full h-[0.025rem] bg-gray-200'></div>
+                </div>
+
+                <form action="/login" className="w-full max-w-sm">
+                    <div className="mb-6 in">
+                        <label htmlFor="email">Email address</label>
+                        <input type="email" id="email" autoComplete='on' required="" />
+                    </div>
+                    <div className="mb-6 in">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" autoComplete='on' required="" />
+                    </div>
+                    <button type="submit" className="inline-flex justify-center rounded-lg text-sm py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 w-full">
+                        <span>Sign in to account</span>
+                    </button>
+                    <input type="hidden" name="remember" value="true" />
+                    <p className="my-5 text-center">
+                        <a href="/password/reset" className="text-sm hover:underline">
+                            Forgot password?
+                        </a>
+                    </p>
+                </form>
+            </div>
+            <footer className="relative shrink-0">
+                <div className="space-y-4 text-sm text-gray-900 sm:flex sm:items-center sm:justify-center sm:space-x-4 sm:space-y-0">
+                    <p className="text-center sm:text-left">
+                        Don&apos;t have an account?
+                    </p>
+                    <a className="inline-flex justify-center rounded-lg text-sm py-2.5 px-4 text-slate-900 ring-1 ring-slate-900/10 hover:ring-slate-900/20" href="/register">
+                        <span>Create an Account
+                            <span aria-hidden="true">
+                            </span>
+                        </span>
+                    </a>
+                </div>
+            </footer>
+        </main>
+    )
+}
+
+export default Hero
